ee607123ae7a0fce39ed8a067e4c43360c698ff0
clemsedem <clemsedem@gmail.com>
2021-07-07 09:57:37 +0000

    middleware dir

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,24 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-import { dbUrl } from "../utils/shared";
-import logger from "../logger";
+import { dbUrl } from '../utils/shared';
+import logger from '../logger';
 
 class Connection {
-  private static mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  };
+	private static mongooseOptions: Object = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	};
 
-  static async dbConnect() {
-    try {
-      await mongoose.connect(dbUrl, Connection.mongooseOptions);
-      logger.info("db connected successfully");
-    } catch (error) {
-      logger.error(`Error connecting to db ${error}`);
-    }
-  }
+	static async dbConnect(): Promise<void> {
+		try {
+			await mongoose.connect(dbUrl, Connection.mongooseOptions);
+			logger.info('db connected successfully');
+		} catch (error) {
+			logger.error(`Error connecting to db ${error}`);
+		}
+	}
 }
 
 export default Connection;
