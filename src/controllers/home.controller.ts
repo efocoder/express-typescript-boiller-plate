5216e7b1da471c73bba8285514c536a401557c78
clemsedem <clemsedem@gmail.com>
2021-07-07 11:11:44 +0000

    new

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,14 +1,14 @@
-import { Request, response, Response } from "express";
-import { apiResponse } from "../utils/shared";
+import { Request, Response } from 'express';
+import { apiResponse } from '../utils/shared';
 
 class HomeController {
-  static hello(req: Request, res: Response) {
-    res.status(200).send(apiResponse(200, "Hello World"));
-  }
+	static hello(req: Request, res: Response) {
+		res.status(200).send(apiResponse(200, 'Hello World'));
+	}
 
-  static try(req: Request, res: Response) {
-    res.status(200).send(apiResponse(200, "Hello from Trial", [1, 3, 5]));
-  }
+	static try(req: Request, res: Response) {
+		res.status(200).send(apiResponse(200, 'Hello from Trial', [1, 3, 5]));
+	}
 }
 
 export default HomeController;
